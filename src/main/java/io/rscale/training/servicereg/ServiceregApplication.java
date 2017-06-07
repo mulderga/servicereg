e22768f1477bb38f0ec42f76e3efab014a635761
Gerard Mulder <g.mulder@cgi.com>
2017-06-07 09:46:35 -0600

    Initial version with pipeline

--- src/main/java/io/rscale/training/servicereg/ServiceregApplication.java
+++ src/main/java/io/rscale/training/servicereg/ServiceregApplication.java
@@ -2,7 +2,9 @@ package io.rscale.training.servicereg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class ServiceregApplication {
 
