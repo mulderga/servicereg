8340f9e93e4a2e2f97de71effef321c67558872b
Gerard Mulder <g.mulder@cgi.com>
2017-06-07 09:30:49 -0600

    Initial version

---
+++ src/main/java/io/rscale/training/servicereg/ServiceregApplication.java
@@ -0,0 +1,12 @@
+package io.rscale.training.servicereg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceregApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceregApplication.class, args);
+	}
+}
